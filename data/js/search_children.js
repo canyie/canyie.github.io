dc4aa086ae31f1d5186522279cec045e9ef5dc34
canyie <a1364259@163.com>
2021-04-10 13:47:24 +0800

    Site updated: 2021-04-10 13:47:22

--- data/js/search_children.js
+++ data/js/search_children.js
@@ -1,1 +1,1 @@
-var domain='http://qzonestyle.gtimg.cn/qzone_v6/lostchild/';document.write('<script type="text/javascript" src="'+domain+'data.js"></script>');document.write('<script type="text/javascript" src="'+domain+'page.js"></script>');/*  |xGv00|2c679710685b04aaa3216bae2b68a58c */
+var domain="http://qzonestyle.gtimg.cn/qzone_v6/lostchild/";document.write('<script type="text/javascript" src="'+domain+'data.js"><\/script>'),document.write('<script type="text/javascript" src="'+domain+'page.js"><\/script>');
